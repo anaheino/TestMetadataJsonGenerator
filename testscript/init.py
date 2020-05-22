82cb4f95806436075f61336ead3817603db28cc5
anaheino <anaheino@gmail.com>
2020-05-22 11:13:02 +0300

    added data json file generator

---
+++ testscript/init.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+import json
+import random
+import sys
+
+def main():
+   generate_testdata()
+
+def generate_testdata():
+   test_units = ['metric', 'celsius', 'electricity used']
+   print('Generating test metadata. Default test units are: ' + ', '.join(test_units))
+   
+   if len(sys.argv) > 1:
+      argument_list = sys.argv[1:]
+      print('Passed arguments. Arguments are used as units for generation of metadata.')
+      print('Arguments passed were: ' + ', '.join(argument_list))
+      test_units = argument_list
+   
+   statistic_list = []
+   
+   for i in [random.randint(i, 25) for i in range(0, 15)]:
+       statistic_list.append(json.dumps(generateStatistic(test_units[random.randint(0, len(test_units)-1)], i)))
+   print('Generated following mock data: ' + ', '.join(statistic_list))
+   
+   with open('execution_metadata.json', 'w') as outfile:
+       json.dump(statistic_list, outfile)
+   print('Dumped results to execution_metadata.json.')
+
+def generateStatistic(unit, value):
+   value_dict = {"unit": unit,
+   "value": value,
+   "timestamp": "10.20.2020",
+   "transactionId": "23"
+   } 
+   return value_dict
+
+main()
